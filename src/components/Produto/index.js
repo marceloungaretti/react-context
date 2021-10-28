f6e2cd7e365911e07cb0d602a976705d1166e2aa
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-27 23:41:52 -0300

    fim do capitulo 4

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -3,7 +3,7 @@ import { memo } from 'react';
 import { IconButton } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-
+import { useCarrinhoContext } from 'common/context/Carrinho';
 
 function Produto({
   nome,
@@ -12,28 +12,32 @@ function Produto({
   valor,
   unidade
 }) {
+  const { carrinho, adicionarProduto } = useCarrinhoContext();
+  const produtoNoCarrinho = carrinho.find(itemDoCarrinho => itemDoCarrinho.id === id);
+
   return (
-      <Container>
-        <div>
-          <img
-            src={`/assets/${foto}.png`}
-            alt={`foto de ${nome}`}
-          />
-          <p>
-            {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
-          </p>
-        </div>
-        <div>
-          <IconButton
-            color="secondary"
-          >
-            <RemoveIcon />
-          </IconButton>
-          <IconButton>
-            <AddIcon />
-          </IconButton>
-        </div>
-      </Container>
+    <Container>
+      <div>
+        <img
+          src={`/assets/${foto}.png`}
+          alt={`foto de ${nome}`}
+        />
+        <p>
+          {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
+        </p>
+      </div>
+      <div>
+        <IconButton
+          color="secondary"
+        >
+          <RemoveIcon />
+        </IconButton>
+        {produtoNoCarrinho?.quantidade || 0}
+        <IconButton onClick={() => adicionarProduto({ nome, foto, id, valor })}>
+          <AddIcon />
+        </IconButton>
+      </div>
+    </Container>
   )
 }
 
